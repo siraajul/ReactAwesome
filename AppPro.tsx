26eb00adb68d09cb386ef59526d81586b6ccdf48
siraajul <shirajulislamparvez@gmail.com>
2024-04-15 05:25:54 +0600

    Add --> Another File for Better Understanding

---
+++ AppPro.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+
+function AppPro() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default AppPro;
