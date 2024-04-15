84353b8f4bc6f1789ef13260ed800a736376ef42
siraajul <shirajulislamparvez@gmail.com>
2024-04-15 06:23:55 +0600

    Finishing

--- AppPro.tsx
+++ AppPro.tsx
@@ -8,14 +8,31 @@ import {
   useColorScheme,
 } from 'react-native';
 
-function AppPro() {
+function AppPro(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <View>
-        <Text>Hello</Text>
+        <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+          Hello World!
+        </Text>
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  },
+  whiteText: {
+    color: '#FFFFFF',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
 
 export default AppPro;
